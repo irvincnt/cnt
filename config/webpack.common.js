dec1155a81e6041a632065c0f5c6e2f76ecc3f4d
irvincnt <irvincnt93@gmail.com>
2021-05-20 16:24:46 -0500

    Agregar tipografias

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -18,6 +18,10 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource'
       }
     ]
   },
