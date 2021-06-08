191578461c723d72af570d8ae84894d140791e53
irvincnt <irvincnt93@gmail.com>
2021-06-08 14:43:38 -0500

    Login, Register | Se agregan validaciones en el formulario y se obtienen los datos

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,49 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from '../hooks/useForm'
 import '../styles/login.scss'
 
 export default function Login () {
+  const [values, handleInputChange] = useForm({
+    email: '',
+    password: ''
+  })
+
+  const handlerRegister = (e) => {
+    e.preventDefault()
+    console.log(values)
+  }
+
   return (
     <div className='login'>
       <div className='wrapper'>
         <div className='login-head'>
           <p>Login</p>
           <span>
-            o
-            <Link to='/register'>Crear una cuenta</Link>
+            o<Link to='/register'>Crear una cuenta</Link>
           </span>
         </div>
         <div className='login-form'>
-          <form>
+          <form onSubmit={handlerRegister}>
             <div className='form-group'>
-              <input className='form-control' type='email' placeholder='Email address' />
+              <input
+                className='form-control'
+                type='email'
+                placeholder='Email address'
+                name='email'
+                value={values.email}
+                onChange={handleInputChange}
+              />
             </div>
             <div className='form-group'>
-              <input className='form-control' type='password' placeholder='Password' />
+              <input
+                className='form-control'
+                type='password'
+                placeholder='Password'
+                name='password'
+                value={values.password}
+                onChange={handleInputChange}
+              />
             </div>
             <div className='form-group'>
               <button className='btn'>Log in</button>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,14 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useForm } from '../hooks/useForm'
+import validator from 'validator'
 
 export default function Register () {
-  const [values, handleInputChange] = useForm({ name: '', email: '', password1: '', password2: '' })
+  const [errors, setErrors] = useState({})
+  const [values, handleInputChange] = useForm({
+    name: '',
+    email: '',
+    password1: '',
+    password2: ''
+  })
 
   const handlerRegister = (e) => {
     e.preventDefault()
     console.log(values)
+
+    const errorsForm = validate()
+    if (Object.keys(errorsForm).length > 0) {
+      setErrors(errorsForm)
+    } else {
+      setErrors({})
+    }
   }
+
+  const validate = () => {
+    const errors = {}
+    if (values.name.trim().length === 0) {
+      errors.name = 'Error en name'
+    }
+
+    if (!validator.isEmail(values.email)) {
+      errors.email = 'Error en email'
+    }
+
+    if (values.password1.length < 8) {
+      errors.password1 = 'Password must be 8 or more characters'
+    } else if (values.password1 !== values.password2) {
+      errors.password1 = 'Password not match'
+      errors.password2 = 'Password not match'
+    }
+
+    return errors
+  }
+
   return (
     <div className='register'>
       <div className='wrapper'>
@@ -22,18 +57,69 @@ export default function Register () {
         <div className='register-form'>
           <form onSubmit={handlerRegister}>
             <div className='form-group'>
-              <input className='form-control is-invalid' type='text' placeholder='Nombre' name='name' value={values.name} onChange={handleInputChange} />
-              <p className='message-invalid'>Error en nombre</p>
+              <input
+                className={`${
+                  errors.name ? 'form-control is-invalid' : 'form-control'
+                }`}
+                type='text'
+                placeholder='Nombre'
+                name='name'
+                value={values.name}
+                onChange={handleInputChange}
+              />
+              {errors.name && (
+                <p className='message-invalid'>{errors.name}</p>
+              )}
             </div>
             <div className='form-group'>
-              <input className='form-control' type='email' placeholder='Correo' name='email' value={values.email} onChange={handleInputChange} />
+              <input
+                className={`${
+                  errors.email ? 'form-control is-invalid' : 'form-control'
+                }`}
+                type='email'
+                placeholder='Correo'
+                name='email'
+                value={values.email}
+                onChange={handleInputChange}
+              />
+              {errors.email && (
+                <p className='message-invalid'>{errors.email}</p>
+              )}
             </div>
             <div className='form-row'>
               <div className='form-group'>
-                <input className='form-control' type='password' placeholder='contraseña' name='password1' value={values.password1} onChange={handleInputChange} />
+                <input
+                  className={`${
+                    errors.password1
+                      ? 'form-control is-invalid'
+                      : 'form-control'
+                  }`}
+                  type='password'
+                  placeholder='contraseña'
+                  name='password1'
+                  value={values.password1}
+                  onChange={handleInputChange}
+                />
+                {errors.password1 && (
+                  <p className='message-invalid'>{errors.password1}</p>
+                )}
               </div>
               <div className='form-group'>
-                <input className='form-control' type='password' placeholder='Conformar contraseña' name='password2' value={values.password2} onChange={handleInputChange} />
+                <input
+                  className={`${
+                    errors.password2
+                      ? 'form-control is-invalid'
+                      : 'form-control'
+                  }`}
+                  type='password'
+                  placeholder='Conformar contraseña'
+                  name='password2'
+                  value={values.password2}
+                  onChange={handleInputChange}
+                />
+                {errors.password2 && (
+                  <p className='message-invalid'>{errors.password2}</p>
+                )}
               </div>
             </div>
             <div className='form-group'>
